7ece31f1fa5075d03a7c7d807b102442f825daeb
LukvonStrom <lukvonstrom@web.de>
2015-10-07 18:25:55 +0200

    Some Updates
    MySQLi, using a config now, brought all content except of the config  to
    one file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/*
+This is the Configuration File. Please insert your data here
+*/
+$dbhost = 'localhost'; //Your DB Hostname.
+$dbuser = 'root'; //Your DB Username.
+$dbpass = '12345'; //Your DB Password.
+$dbname = 'minecraft'; //Your DB Name.
