fba2f308465313fffc219b1a94b8b2c3fdc23c6c
Stefano <stetto98@hotmail.it>
2015-10-25 18:12:50 +0100

    New debug feature
    Added a new debug feature

--- config.php
+++ config.php
@@ -6,3 +6,6 @@ $dbhost = 'localhost'; //Your DB Hostname.
 $dbuser = 'root'; //Your DB Username.
 $dbpass = '12345'; //Your DB Password.
 $dbname = 'minecraft'; //Your DB Name.
+
+
+$debug = 'false'; //Debug mode, set to 'true' to activate.
